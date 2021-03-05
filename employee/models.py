76ca544b9d4618723c66c401942eb6da8a54ee9e
sankar-guru <sankar@yaane.in>
2021-03-05 13:36:43 +0530

    second commit

--- employee/models.py
+++ employee/models.py
@@ -15,7 +15,7 @@ class Employee(models.Model):
 
     first_name = models.CharField(_("first Name"), max_length=50,blank=True,null=True)
     last_name = models.CharField(_("last Name"),max_length=20,blank=True,null=True)
-    username = models.CharField(_("username"),max_length=20, unique=True,blank=True,null=True)
+    username = models.CharField(_("username"),max_length=20,blank=True,null=True)
     phone_no = models.CharField(_("phoneno"),max_length=16,validators=[Phone_number_validator],blank=True,null=True)
     email = models.CharField(_("Email"),max_length=25,unique=True,validators=[emailvalidator],blank=True,null=True)
     description = models.TextField(_("Description"))
